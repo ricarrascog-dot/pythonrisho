7b3b657f4cb0cfa99c68a879816ddd369e0f04e7
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-05-05 19:37:52 -0400

    tercer cambio

---
+++ python0505.py
@@ -0,0 +1,15 @@
+diccionario = {"Nombre": "Cesar Huispe","fonos": [9877882,963503690,8777792],"activo": True} 
+#
+print ("Nombre", diccionario["Nombre"])
+print ("Segundo Telefono",diccionario["fonos"][1]) 
+#Inserción 
+diccionario["email"]="cesar.huispe@example.com"
+diccionario["fonos"].append(123456) 
+#Actualizacion
+diccionario["activo"]=False
+diccionario["fonos"][0]=999999999
+#Eliminación 
+del diccionario["activo"]
+diccionario["fonos"].pop(2)
+
+print(diccionario)
