267ba5041c3431738acc9ff92bc9673e688f5703
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-05-05 20:59:34 -0400

    funciones 4

---
+++ funciones 4.py
@@ -0,0 +1,12 @@
+def suma():
+    a = 5
+    b = 3
+    return (a+b) 
+def suma(a,b):
+    print (a+b)
+    return (a+b)
+suma(10, 5)
+otra_suma = suma(16, 9)
+print(otra_suma)
+#otra_suma=suma(6, 7)
+#print(otra_suma)    
