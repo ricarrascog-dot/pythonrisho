0a2ceaa086b22442502d1707570aceb614efe0cb
Ricardo <ri.carrascog@duocuc.cl>
2026-05-12 21:21:56 -0400

    actividad 12 mayo

---
+++ mainn.py
@@ -0,0 +1,22 @@
+import utils as u
+
+personas = []
+while True: 
+    u.mostrar_menu()
+   
+
+    try: 
+        opcion = int(input("Ingrese una opción: "))
+    except:
+        print("La opcion debe numérica")
+        opcion = 0  
+     
+    if opcion == 1: 
+        print("Opción 1 seleccionada")
+    elif opcion == 2:
+        print("Esta es la opcion 2")
+    elif opcion == 3: 
+        print("Gracias por usar el sistema")
+        break 
+    else:
+        print("Opción no válida, por favor intente de nuevo")          
---
+++ utils.py
@@ -0,0 +1,32 @@
+def mostrar_menu():    
+    print("*****Menu*****") 
+    print("1) Agregar persona")
+    print("2) Listar personas")
+    print("3) Salir")  
+def opcion_menu():
+    flag_opcion = True
+    opcion = 0
+    while flag_opcion:
+        try: 
+            opcion = int(input("Ingrese una opción: "))
+            flag_opcion = False
+        except:
+            print("La opcion debe numérica")
+    return opcion    
+def ingresar_persona():
+    nombre = validacion_texto("Ingrese el nombre de la persona: ")
+    correo = validacion_texto("Ingrese el correo de la persona: ")
+
+    usuario = {"nombre": nombre, "correo": correo}
+    return usuario
+
+def validacion_texto(texto_a_mostrar):
+    flag_texto = True
+    texto = ""
+    while flag_texto == True:
+        texto = input(texto_a_mostrar)
+        if texto != "":
+            flag_texto = False
+        else:
+            print("El texto no puede estar vacio")
+    return texto        
